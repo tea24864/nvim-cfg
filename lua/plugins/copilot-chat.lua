dbebaff08d12b80f447d2056fb30c0b6fdaee369
Tim Chung <tea24864@gmail.com>
2024-05-06 01:31:25 -0500

    feat(copilot-chat): enhance plugin configuration and add new keybindings
    This commit enhances the configuration options for the CopilotChat
    plugin, including options for help text, debug mode, language settings,
    and more. It also adds new keybindings for various actions such as
    explaining code, toggling the chat window, saving/loading chat history,
    generating tests, reviewing code, fixing diagnostics, and resetting chat
    history.

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -3,13 +3,40 @@ return {
     "CopilotC-Nvim/CopilotChat.nvim",
     branch = "canary",
     dependencies = {
-      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
-      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+      { "zbirenbaum/copilot.lua" },
+      { "nvim-lua/plenary.nvim" },
     },
     opts = {
-      debug = true, -- Enable debugging
-      -- See Configuration section for rest
+      show_help = "yes", -- Show help text for CopilotChatInPlace, default: yes
+      debug = false, -- Enable or disable debug mode, the log file will be in ~/.local/state/nvim/CopilotChat.nvim.log
+      disable_extra_info = "no", -- Disable extra information (e.g: system prompt) in the response.
+      language = "English", -- Copilot answer language settings when using default prompts. Default language is English.
+      -- proxy = "socks5://127.0.0.1:3000", -- Proxies requests via https or socks.
+      -- temperature = 0.1,
     },
-    -- See Commands section for default commands if you want to lazy load on them
+    build = function()
+      vim.notify("Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim.")
+    end,
+    event = "VeryLazy",
+    keys = {
+      { "<leader>cci", ":CopilotChat ", desc = "CopilotChat - Input Question" },
+      { "<leader>cce", "<cmd>CopilotChatExplain<cr>", desc = "CopilotChat - Explain code" },
+      { "<leader>ccc", ":CopilotChatToggle<cr>", desc = "CopilotChat - Toggle chat window" },
+      { "<leader>cco", ":CopilotChatOpen<cr>", desc = "CopilotChat - Open chat window" },
+      { "<leader>ccs", ":CopilotChatSave ", desc = "CopilotChat - Save chat history" },
+      { "<leader>ccl", ":CopilotChatLoad ", desc = "CopilotChat - Load chat history" },
+      { "<leader>cct", "<cmd>CopilotChatTests<cr>", desc = "CopilotChat - Generate tests" },
+      { "<leader>ccr", "<cmd>CopilotChatReview<cr>", desc = "CopilotChat - Review the selected code" },
+      {
+        "<leader>ccf",
+        "<cmd>CopilotChatFixDiagnostic<cr>", -- Get a fix for the diagnostic message under the cursor.
+        desc = "CopilotChat - Fix diagnostic",
+      },
+      {
+        "<leader>ccr",
+        "<cmd>CopilotChatReset<cr>", -- Reset chat history and clear buffer.
+        desc = "CopilotChat - Reset chat history and clear buffer",
+      },
+    },
   },
 }
